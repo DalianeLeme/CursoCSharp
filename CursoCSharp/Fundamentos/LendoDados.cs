6701ae7770c181f1b7846ee6a634ee4b541bb8c8
Daliane <daliane.leme@hotmail.com>
2022-11-16 22:38:20 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharp/Fundamentos/LendoDados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoCSharp.Fundamentos
+{
+    class LendoDados
+    {
+        public static void Executar()
+        {
+            Console.Write("Qual é o seu nome? ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Qual é a sua idade? ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o seu salário? ");
+            double salario = double.Parse(Console.ReadLine(),
+                CultureInfo.InvariantCulture); // Não vai usar o simbolo do sistema operacional e sim o "."
+
+            Console.WriteLine($"{nome} {idade} R${salario}");
+            
+        }
+    }
+}
