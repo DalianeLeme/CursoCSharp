bc0903c5cd0323ebafebaf8676468b93a5e084c4
Daliane <daliane.leme@hotmail.com>
2022-11-21 18:42:08 -0300

    feat: estudos de: * Operadores de Atribuição * Operadores Unários * Operador Ternário

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -19,6 +19,9 @@ namespace CursoCSharp {
                 {"Operadores Aritméticos - Fundamentos", OperadoresAritmeticos.Executar},
                 {"Operadores Relacionais - Fundamentos", OperadoresRelacionais.Executar},
                 {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar},
+                {"Operadores de Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
+                {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar},
+                {"Operador Ternário - Fundamentos", OperadorTernario.Executar},
             });
 
             central.SelecionarEExecutar();
